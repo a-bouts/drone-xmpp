fabfcaa1ed5b81cfb83d198f65e4f79b577268a5
Eve <eve@no-cloud.fr>
2017-07-13 16:56:35 +0200

    Fork appleboy/gtalk drone plugin

--- src/drone-xmpp/main.go
+++ src/drone-xmpp/main.go
@@ -1,60 +1,163 @@
 package main
 
 import (
-	"crypto/tls"
-        "fmt"
-	"github.com/mattn/go-xmpp"
-	"log"
 	"os"
-	"strings"
+
+	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/urfave/cli"
 )
 
-func serverName(host string) string {
-	return strings.Split(host, ":")[0]
-}
+// Version set at compile-time
+var Version string
 
 func main() {
-
-	message := fmt.Sprintf("[%s] <%s> (%s)『%s』by %s", os.Getenv("DRONE_BUILD_STATUS"), os.Getenv("DRONE_BUILD_LINK"), os.Getenv("DRONE_COMMIT_BRANCH"), os.Getenv("DRONE_COMMIT_MESSAGE"), os.Getenv("DRONE_COMMIT_AUTHOR"));
-
-	log.Print("Sending message " + message);
-
-
-	var jid = os.Getenv("XMPP_JID")
-	var password = os.Getenv("XMPP_PASSWORD")
-	var to = os.Getenv("XMPP_TO")
-
-
-	var host string
-	host, ok := os.LookupEnv("XMPP_HOST")
-	if !ok {
-		host = strings.Split(jid, "@")[1]
+	app := cli.NewApp()
+	app.Name = "xmpp plugin"
+	app.Usage = "xmpp plugin"
+	app.Action = run
+	app.Version = Version
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "xmpp.host",
+			Usage:  "xmpp host",
+			EnvVar: "PLUGIN_XMPP_HOST,XMPP_HOST",
+		},
+		cli.StringFlag{
+			Name:   "xmpp.jid",
+			Usage:  "xmpp jid",
+			EnvVar: "PLUGIN_XMPP_JID,XMPP_JID",
+		},
+		cli.StringFlag{
+			Name:   "xmpp.password",
+			Usage:  "xmpp password",
+			EnvVar: "PLUGIN_XMPP_PASSWORD,XMPP_PASSWORD",
+		},
+		cli.StringSliceFlag{
+			Name:   "xmpp.to",
+			Usage:  "send message to user",
+			EnvVar: "PLUGIN_XMPP_TO,XMPP_TO",
+		},
+		cli.StringSliceFlag{
+			Name:   "message",
+			Usage:  "xmpp message",
+			EnvVar: "PLUGIN_MESSAGE",
+		},
+		cli.StringFlag{
+			Name:   "repo.owner",
+			Usage:  "repository owner",
+			EnvVar: "DRONE_REPO_OWNER",
+		},
+		cli.StringFlag{
+			Name:   "repo.name",
+			Usage:  "repository name",
+			EnvVar: "DRONE_REPO_NAME",
+		},
+		cli.StringFlag{
+			Name:   "commit.sha",
+			Usage:  "git commit sha",
+			EnvVar: "DRONE_COMMIT_SHA",
+		},
+		cli.StringFlag{
+			Name:   "commit.branch",
+			Value:  "master",
+			Usage:  "git commit branch",
+			EnvVar: "DRONE_COMMIT_BRANCH",
+		},
+		cli.StringFlag{
+			Name:   "commit.author",
+			Usage:  "git author name",
+			EnvVar: "DRONE_COMMIT_AUTHOR",
+		},
+		cli.StringFlag{
+			Name:   "commit.author.email",
+			Usage:  "git author email",
+			EnvVar: "DRONE_COMMIT_AUTHOR_EMAIL",
+		},
+		cli.StringFlag{
+			Name:   "commit.message",
+			Usage:  "commit message",
+			EnvVar: "DRONE_COMMIT_MESSAGE",
+		},
+		cli.StringFlag{
+			Name:   "build.event",
+			Value:  "push",
+			Usage:  "build event",
+			EnvVar: "DRONE_BUILD_EVENT",
+		},
+		cli.IntFlag{
+			Name:   "build.number",
+			Usage:  "build number",
+			EnvVar: "DRONE_BUILD_NUMBER",
+		},
+		cli.StringFlag{
+			Name:   "build.status",
+			Usage:  "build status",
+			Value:  "success",
+			EnvVar: "DRONE_BUILD_STATUS",
+		},
+		cli.StringFlag{
+			Name:   "build.link",
+			Usage:  "build link",
+			EnvVar: "DRONE_BUILD_LINK",
+		},
+		cli.StringFlag{
+			Name:   "build.tag",
+			Usage:  "build tag",
+			EnvVar: "DRONE_TAG",
+		},
+		cli.Float64Flag{
+			Name:   "job.started",
+			Usage:  "job started",
+			EnvVar: "DRONE_JOB_STARTED",
+		},
+		cli.Float64Flag{
+			Name:   "job.finished",
+			Usage:  "job finished",
+			EnvVar: "DRONE_JOB_FINISHED",
+		},
+		cli.StringFlag{
+			Name:   "env-file",
+			Usage:  "source env file",
+			EnvVar: "ENV_FILE",
+		},
 	}
 
-	var talk *xmpp.Client
-	var err error
-	options := xmpp.Options {
-		Host:          host,
-		User:          jid,
-		Password:      password,
-		NoTLS:         true,
-		StartTLS:      true,
-        	TLSConfig: &tls.Config{
-            		ServerName: serverName(host),
-            		InsecureSkipVerify: false,
-        	},
-		Debug:         false,
-		Session:       false,
+	app.Run(os.Args)
+}
+
+func run(c *cli.Context) error {
+	if c.String("env-file") != "" {
+		_ = godotenv.Load(c.String("env-file"))
 	}
 
-	talk, err = options.NewClient()
-
-	if err != nil {
-		log.Fatal(err)
+	plugin := Plugin{
+		Repo: Repo{
+			Owner: c.String("repo.owner"),
+			Name:  c.String("repo.name"),
+		},
+		Build: Build{
+			Tag:      c.String("build.tag"),
+			Number:   c.Int("build.number"),
+			Event:    c.String("build.event"),
+			Status:   c.String("build.status"),
+			Commit:   c.String("commit.sha"),
+			Branch:   c.String("commit.branch"),
+			Author:   c.String("commit.author"),
+			Email:    c.String("commit.author.email"),
+			Message:  c.String("commit.message"),
+			Link:     c.String("build.link"),
+			Started:  c.Float64("job.started"),
+			Finished: c.Float64("job.finished"),
+		},
+		Config: Config{
+			Host:       c.String("xmpp.host"),
+			Jid:        c.String("xmpp.username"),
+			Password:   c.String("xmpp.password"),
+			To:         c.StringSlice("xmpp.to"),
+			Message:    c.StringSlice("message"),
+		},
 	}
 
-	_, err = talk.Send(xmpp.Chat{Remote: to, Type: "chat", Text: message})
-	if err != nil {
-		log.Fatal(err)
-	}
+	return plugin.Exec()
 }
---
+++ src/drone-xmpp/plugin.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/appleboy/drone-facebook/template"
+	"github.com/mattn/go-xmpp"
+)
+
+type (
+	// Repo information.
+	Repo struct {
+		Owner string
+		Name  string
+	}
+
+	// Build information.
+	Build struct {
+		Tag      string
+		Event    string
+		Number   int
+		Commit   string
+		Message  string
+		Branch   string
+		Author   string
+		Email    string
+		Status   string
+		Link     string
+		Started  float64
+		Finished float64
+	}
+
+	// Config for the plugin.
+	Config struct {
+		Host       string
+		Jid        string
+		Password   string
+		To         []string
+		Message    []string
+	}
+
+	// Plugin values.
+	Plugin struct {
+		Repo   Repo
+		Build  Build
+		Config Config
+	}
+)
+
+func trimElement(keys []string) []string {
+	var newKeys []string
+
+	for _, value := range keys {
+		value = strings.Trim(value, " ")
+		if len(value) == 0 {
+			continue
+		}
+		newKeys = append(newKeys, value)
+	}
+
+	return newKeys
+}
+
+func serverName(host string) string {
+	return strings.Split(host, ":")[0]
+}
+
+// Exec executes the plugin.
+func (p Plugin) Exec() error {
+
+	if len(p.Config.Jid) == 0 || len(p.Config.Password) == 0 || len(p.Config.To) == 0 {
+		log.Println("missing xmpp config")
+
+		return errors.New("missing xmpp config")
+	}
+
+        if len(p.Config.Host) == 0 {
+                p.Config.Host = serverName(p.Config.Jid)
+        }
+
+	var message []string
+	if len(p.Config.Message) > 0 {
+		message = p.Config.Message
+	} else {
+		message = p.Message(p.Repo, p.Build)
+	}
+
+	xmpp.DefaultConfig = tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	options := xmpp.Options{
+		Host:          p.Config.Host,
+		User:          p.Config.Jid,
+		Password:      p.Config.Password,
+		NoTLS:         false,
+		StartTLS:      true,
+		Debug:         false,
+		Session:       false,
+		Status:        "xa",
+		StatusMessage: "I for one welcome our new codebot overlords.",
+	}
+
+	talk, err := options.NewClient()
+
+	if err != nil {
+		log.Println(err.Error())
+
+		return err
+	}
+
+	// send message.
+	for _, user := range p.Config.To {
+		for _, value := range trimElement(message) {
+			txt, err := template.RenderTrim(value, p)
+			if err != nil {
+				return err
+			}
+
+			talk.Send(xmpp.Chat{Remote: user, Type: "chat", Text: txt})
+		}
+	}
+
+	return nil
+}
+
+// Message is plugin default message.
+func (p Plugin) Message(repo Repo, build Build) []string {
+	return []string{fmt.Sprintf("[%s] <%s> (%s)『%s』by %s",
+		build.Status,
+		build.Link,
+		build.Branch,
+		build.Message,
+		build.Author,
+	)}
+}
